27c056defeeb2b30b13345598aeb8b6cea9cdcdb
GKoefender <gustavokoefender1999@hotmail.com>
2022-05-20 15:17:58 -0300

    home page

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss'
+
+function SubscribeButton () {
+  return (
+    <button className={styles.container}>
+      <span>Subscribe now</span>
+    </button>
+  )
+}
+
+export { SubscribeButton }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import Head from 'next/head'
 
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import styles from './home.module.scss'
+
 export default function Home () {
   return (
     <>
       <Head>
         <link rel="shortcut icon" href="/favicon.png" />
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for R$9,98 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg"></img>
+      </main>
     </>
   )
 }
