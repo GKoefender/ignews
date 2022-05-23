fdcabb6f1d97cd3cbb613480b82707e77f6fe5ae
GKoefender <gustavokoefender1999@hotmail.com>
2022-05-23 15:44:13 -0300

    redirecionando para a tela de pagamento

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/react'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe.js'
 import styles from './styles.module.scss'
 
 interface subscribeButtonProps {
@@ -5,8 +8,28 @@ interface subscribeButtonProps {
 }
 
 function SubscribeButton ({ priceId }: subscribeButtonProps) {
+  const { data: session } = useSession()
+
+  async function handleSubscribe () {
+    if (!session) {
+      signIn('github')
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return (
-    <button className={styles.container}>
+    <button className={styles.container} onClick={handleSubscribe}>
       <span>Subscribe now</span>
     </button>
   )
